b6fd31eb997ffd951405e6cedc76e86ac82a85bd
Samuel Taylor <samtaylor.uk@gmail.com>
2017-03-14 14:39:26 +0000

    Inital Commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM alpine:3.5
+
+# Set environment
+ENV SERVICE_NAME=traefik \
+    SERVICE_HOME=/app/traefik \
+    SERVICE_VERSION=v1.2.0-rc2 \
+    SERVICE_URL=https://github.com/containous/traefik/releases/download
+ENV SERVICE_RELEASE=${SERVICE_URL}/${SERVICE_VERSION}/traefik_linux-amd64 \
+    PATH=${PATH}:${SERVICE_HOME}/bin
+
+ADD *.sh $SERVICE_HOME/bin/
+
+# Download and install traefik
+RUN mkdir -p ${SERVICE_HOME}/bin ${SERVICE_HOME}/etc ${SERVICE_HOME}/log ${SERVICE_HOME}/certs ${SERVICE_HOME}/acme && \
+    apk --no-cache --no-progress add ca-certificates && \
+    apk --no-cache --no-progress --virtual=.build-dependencies add wget && \
+    wget -q "${SERVICE_RELEASE}" -O "${SERVICE_HOME}/bin/traefik" && \
+    touch ${SERVICE_HOME}/etc/rules.toml && \
+    chmod +x ${SERVICE_HOME}/bin/traefik && \
+    chmod +x ${SERVICE_HOME}/bin/*.sh && \
+    apk --no-progress del .build-dependencies
+
+EXPOSE 80 443
+
+WORKDIR $SERVICE_HOME
+
+ENTRYPOINT $SERVICE_HOME/bin/entrypoint.sh
+
+CMD ["$SERVICE_HOME/bin/traefik", "--configFile=$SERVICE_HOME/etc/traefik.toml"]
---
+++ traefik.toml.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env sh
+
+TRAEFIK_HTTP_PORT=${TRAEFIK_HTTP_PORT:-"80"}
+TRAEFIK_HTTPS_ENABLE=${TRAEFIK_HTTPS_ENABLE:-"false"}
+TRAEFIK_HTTPS_PORT=${TRAEFIK_HTTPS_PORT:-"443"}
+TRAEFIK_ADMIN_PORT=${TRAEFIK_ADMIN_PORT:-"8000"}
+TRAEFIK_LOG_LEVEL=${TRAEFIK_LOG_LEVEL:-"INFO"}
+TRAEFIK_SSL_PATH=${TRAEFIK_SSL_PATH:-"${SERVICE_HOME}/certs"}
+TRAEFIK_ACME_ENABLE=${TRAEFIK_ACME_ENABLE:-"false"}
+TRAEFIK_ACME_EMAIL=${TRAEFIK_ACME_EMAIL:-"test@traefik.io"}
+TRAEFIK_ACME_ONDEMAND=${TRAEFIK_ACME_ONDEMAND:-"true"}
+TRAEFIK_ACME_ONHOSTRULE=${TRAEFIK_ACME_ONHOSTRULE:-"true"}
+TRAEFIK_K8S_ENABLE=${TRAEFIK_K8S_ENABLE:-"false"}
+TRAEFIK_K8S_OPTS=${TRAEFIK_K8S_OPTS:-""}
+TRAEFIK_RANCHER_ENABLE=${TRAEFIK_RANCHER_ENABLE:-"false"}
+TRAEFIK_RANCHER_DOMAIN=${TRAEFIK_RANCHER_DOMAIN:-"rancher.internal"}
+TRAEFIK_RANCHER_ENDPOINT=${TRAEFIK_RANCHER_ENDPOINT:-""}
+TRAEFIK_RANCHER_ACCESSKEY=${TRAEFIK_RANCHER_ACCESSKEY:-""}
+TRAEFIK_RANCHER_SERCETKEY=${TRAEFIK_RANCHER_SERCETKEY:-""}
+TRAEFIK_RANCHER_OPTS=${TRAEFIK_RANCHER_OPTS:-""}
+
+TRAEFIK_ENTRYPOINTS_HTTP="\
+  [entryPoints.http]
+  address = \":${TRAEFIK_HTTP_PORT}\"
+"
+
+filelist=`ls -1 ${TRAEFIK_SSL_PATH}/*.key | cut -d"." -f1`
+RC=`echo $?`
+
+if [ $RC -eq 0 ]; then
+    TRAEFIK_ENTRYPOINTS_HTTPS="\
+  [entryPoints.https]
+  address = \":${TRAEFIK_HTTPS_PORT}\"
+    [entryPoints.https.tls]"
+    for i in $filelist; do
+        if [ -f "$i.crt" ]; then
+            TRAEFIK_ENTRYPOINTS_HTTPS=$TRAEFIK_ENTRYPOINTS_HTTPS"
+      [[entryPoints.https.tls.certificates]]
+      certFile = \"${i}.crt\"
+      keyFile = \"${i}.key\"
+"
+        fi
+    done
+fi
+
+if [ "X${TRAEFIK_HTTPS_ENABLE}" == "Xtrue" ]; then
+    TRAEFIK_ENTRYPOINTS_OPTS=${TRAEFIK_ENTRYPOINTS_HTTP}${TRAEFIK_ENTRYPOINTS_HTTPS}
+    TRAEFIK_ENTRYPOINTS='"http", "https"'
+elif [ "X${TRAEFIK_HTTPS_ENABLE}" == "Xonly" ]; then
+    TRAEFIK_ENTRYPOINTS_HTTP=$TRAEFIK_ENTRYPOINTS_HTTP"\
+    [entryPoints.http.redirect]
+       entryPoint = \"https\"
+"
+    TRAEFIK_ENTRYPOINTS_OPTS=${TRAEFIK_ENTRYPOINTS_HTTP}${TRAEFIK_ENTRYPOINTS_HTTPS}
+    TRAEFIK_ENTRYPOINTS='"http", "https"'
+else
+    TRAEFIK_ENTRYPOINTS_OPTS=${TRAEFIK_ENTRYPOINTS_HTTP}
+    TRAEFIK_ENTRYPOINTS='"http"'
+fi
+
+if [ "X${TRAEFIK_K8S_ENABLE}" == "Xtrue" ]; then
+    TRAEFIK_K8S_OPTS="[kubernetes]"
+fi
+
+if [ "X${TRAEFIK_RANCHER_ENABLE}" == "Xtrue" ]; then
+    TRAEFIK_RANCHER_OPTS="\
+[rancher]
+domain = \"${TRAEFIK_RANCHER_DOMAIN}\"
+Watch = true
+
+Endpoint = \"${TRAEFIK_RANCHER_ENDPOINT}\"
+AccessKey = \"${TRAEFIK_RANCHER_ACCESSKEY}\"
+SecretKey = \"${TRAEFIK_RANCHER_SERCETKEY}\"
+
+"
+fi
+
+TRAEFIK_ACME_CFG=""
+if [ "X${TRAEFIK_HTTPS_ENABLE}" == "Xtrue" ] || [ "X${TRAEFIK_HTTPS_ENABLE}" == "Xonly" ] && [ "X${TRAEFIK_ACME_ENABLE}" == "Xtrue" ]; then
+
+    TRAEFIK_ACME_CFG="\
+[acme]
+email = \"${TRAEFIK_ACME_EMAIL}\"
+storage = \"${SERVICE_HOME}/acme/acme.json\"
+onDemand = ${TRAEFIK_ACME_ONDEMAND}
+OnHostRule = ${TRAEFIK_ACME_ONHOSTRULE}
+entryPoint = \"https\"
+
+"
+
+fi
+
+cat << EOF > ${SERVICE_HOME}/etc/traefik.toml
+# traefik.toml
+logLevel = "${TRAEFIK_LOG_LEVEL}"
+
+defaultEntryPoints = [${TRAEFIK_ENTRYPOINTS}]
+[entryPoints]
+${TRAEFIK_ENTRYPOINTS_OPTS}
+[web]
+address = ":${TRAEFIK_ADMIN_PORT}"
+
+${TRAEFIK_K8S_OPTS}
+
+${TRAEFIK_RANCHER_OPTS}
+
+${TRAEFIK_ACME_CFG}
+EOF
