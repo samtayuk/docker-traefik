6b6109dd7107c0c23591975c0fc7c96c3eedabec
Samuel Taylor <samtaylor.uk@gmail.com>
2017-03-14 15:42:33 +0000

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,28 @@ function serviceCheck {
     ${SERVICE_HOME}/bin/traefik.toml.sh
 }
 
+function serviceLog {
+    log "[ Redirecting ${SERVICE_NAME} log... ]"
+    if [ -e ${TRAEFIK_LOG_FILE} ]; then
+        rm ${TRAEFIK_LOG_FILE}
+    fi
+    ln -sf /proc/1/fd/1 ${TRAEFIK_LOG_FILE}
+}
+
+function serviceAccess {
+    log "[ Redirecting ${SERVICE_NAME} log... ]"
+    if [ -e ${TRAEFIK_ACCESS_FILE} ]; then
+        rm ${TRAEFIK_ACCESS_FILE}
+    fi
+    ln -sf /proc/1/fd/1 ${TRAEFIK_ACCESS_FILE}
+}
+
+export TRAEFIK_LOG_FILE=${TRAEFIK_LOG_FILE:-"${SERVICE_HOME}/log/traefik.log"}
+export TRAEFIK_ACCESS_FILE=${TRAEFIK_ACCESS_FILE:-"${SERVICE_HOME}/log/access.log"}
+
 serviceCheck
+serviceLog
+serviceAccess
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
